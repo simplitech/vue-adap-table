4b78961a08e89d1aa153507bbe6490e64c8531a7
Felipe Gibran Eleuterio Toledo <ft.gibran@gmail.com>
2020-04-09 10:21:01 -0300

    style: format

--- src/AdapSearchfield.ts
+++ src/AdapSearchfield.ts
@@ -3,15 +3,32 @@ const template = `
          v-if="collection"
          class="adap-searchfield"
          v-model="collection.search"
-         @keyup="collection.querySearch()"
+         @keyup="search"
   />
 `
 
 import { Component, Prop, Vue } from 'vue-property-decorator'
 import { PageCollection, IResource } from '@simpli/resource-collection'
 
+const debounce = require('lodash.debounce')
+
 @Component({ template })
 export class AdapSearchfield extends Vue {
   @Prop({ required: true })
   collection!: PageCollection<IResource>
+
+  @Prop({ type: Number, default: 1000 })
+  debounceTimer!: number
+
+  get querySearch(): () => Promise<any> {
+    return debounce(() => this.debounceQuerySearch(), this.debounceTimer)
+  }
+
+  async debounceQuerySearch() {
+    await this.collection.querySearch()
+  }
+
+  async search() {
+    await this.querySearch()
+  }
 }
